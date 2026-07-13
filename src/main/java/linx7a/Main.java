1cba79179f8dd0723799c4ab482bf537a2b0e660
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-13 20:16:17 +0300

    Add Main entry point with Spring context bootstrap

--- src/main/java/linx7a/Main.java
+++ src/main/java/linx7a/Main.java
@@ -1,6 +1,13 @@
 package linx7a;
 
+import linx7a.config.AppConfig;
+import linx7a.model.User;
+import linx7a.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
     }
 }
