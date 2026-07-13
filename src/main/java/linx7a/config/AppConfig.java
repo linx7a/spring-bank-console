e99a5661a5b3550a5c701692fc58117568c340b9
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-13 19:47:00 +0300

    Add AccountProperties and AppConfig for Spring context setup

---
+++ src/main/java/linx7a/config/AppConfig.java
@@ -0,0 +1,11 @@
+package linx7a.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan("linx7a")
+public class AppConfig {
+}
