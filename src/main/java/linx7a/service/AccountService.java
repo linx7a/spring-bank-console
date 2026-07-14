8f1cfddb56fdd19eec19aa33da9c7b5a6d86490f
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-14 12:11:52 +0300

    Wire AccountProperties into AccountService createAccount

--- src/main/java/linx7a/service/AccountService.java
+++ src/main/java/linx7a/service/AccountService.java
@@ -1,5 +1,6 @@
 package linx7a.service;
 
+import linx7a.config.AccountProperties;
 import linx7a.model.Account;
 import linx7a.model.User;
 import org.springframework.stereotype.Service;
@@ -11,17 +12,19 @@ import java.util.List;
 public class AccountService {
     private final List<Account> accounts = new ArrayList<>();
     private final UserService userService;
+    private final AccountProperties accountProperties;
 
-
-
-    public AccountService(UserService userService) {
+    public AccountService(UserService userService, AccountProperties accountProperties) {
         this.userService = userService;
+        this.accountProperties = accountProperties;
     }
+
+
     private int idCounter = 0;
     public Account createAccount(int userId) {
         User user = userService.findUserById(userId);
         idCounter++;
-        Account account = new Account(idCounter, user.getId(), 3);
+        Account account = new Account(idCounter, user.getId(), accountProperties.getDefaultAmount());
         accounts.add(account);
         return account;
     }
