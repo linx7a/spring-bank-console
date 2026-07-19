f4f6e93e30117fd66da9b16e4bf0a677fd2ec066
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-19 17:18:05 +0300

    Add ConsoleOperationType enum

---
+++ src/main/java/linx7a/command/ConsoleOperationType.java
@@ -0,0 +1,11 @@
+package linx7a.command;
+
+public enum ConsoleOperationType {
+    USER_CREATE,
+    SHOW_ALL_USERS,
+    ACCOUNT_CREATE,
+    ACCOUNT_CLOSE,
+    ACCOUNT_DEPOSIT,
+    ACCOUNT_TRANSFER,
+    ACCOUNT_WITHDRAW
+}
