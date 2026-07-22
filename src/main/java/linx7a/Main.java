a28439424d9d9eeb09951a04566a04e16478365f
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-22 16:10:11 +0300

    Wire OperationsConsoleListener into Main

--- src/main/java/linx7a/Main.java
+++ src/main/java/linx7a/Main.java
@@ -1,13 +1,15 @@
 package linx7a;
 
+import linx7a.command.OperationsConsoleListener;
 import linx7a.config.AppConfig;
-import linx7a.model.User;
-import linx7a.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        OperationsConsoleListener operationsConsoleListener = context.getBean(OperationsConsoleListener.class);
 
+        Thread thread = new Thread(operationsConsoleListener);
+        thread.start();
     }
 }
