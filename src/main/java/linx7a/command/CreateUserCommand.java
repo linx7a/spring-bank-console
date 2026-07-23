390452acf04eb1c5ca171a4bc83b2cfa7897ff1a
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-07-23 18:09:12 +0300

    Fix CreateUserCommand to link created account to user's accountList

--- src/main/java/linx7a/command/CreateUserCommand.java
+++ src/main/java/linx7a/command/CreateUserCommand.java
@@ -1,6 +1,8 @@
 package linx7a.command;
 
+import linx7a.model.Account;
 import linx7a.model.User;
+import linx7a.service.AccountService;
 import linx7a.service.UserService;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,11 @@ import java.util.Scanner;
 @Component
 public class CreateUserCommand implements OperationCommand{
     private final UserService userService;
+    private final AccountService accountService;
 
-    public CreateUserCommand(UserService userService) {
+    public CreateUserCommand(UserService userService, AccountService accountService) {
         this.userService = userService;
+        this.accountService = accountService;
     }
     private final Scanner scanner = new Scanner(System.in);
     @Override
@@ -19,6 +23,8 @@ public class CreateUserCommand implements OperationCommand{
         System.out.println("Введите логин для создания пользователя:");
         String login = scanner.nextLine();
         User user = userService.createUser(login);
+        Account account = accountService.createAccount(user.getId());
+        user.addAccount(account);
         System.out.println("Пользователь создан: " + user);
     }
 
